e30d018c94da69ad945781799b5613e288f3838a
MB <mbarry323@gmail.com>
2018-05-06 18:49:32 -0400

    LZMR-3 did DTM for SuperSightings, Character, and SuperPower tables

--- Superhero SIghtings SQL script MSS.sql
+++ Superhero SIghtings SQL script MSS.sql
@@ -5,31 +5,45 @@ Add changes, commit, pull, resolve conflicts, commit again (if need to), push
 
 DROP DATABASE IF EXISTS `SuperheroSightings`;
 
--- my changes:
-
-blah blah blah
-
 CREATE DATABASE `SuperheroSightings`;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 USE `SuperheroSightings`;
 
--- my change to master
-=======
-USE `MyDatabase`;
+-- Bridge Table:
+CREATE TABLE IF NOT EXISTS `Character_SuperPower`(
+`SuperPowerID` int not null,
+`CharacterID` int not null,
 
-CREATE TABLE IF NOT EXISTS ();
->>>>>>> 41571b443decff58c53b35ed02c1b3801a88d560
-=======
-USE `SuperheroSightings`;
-
-CREATE TABLE IF NOT EXISTS `Character_Superpower`(
-
-`Character_SuperpowerID` int not null auto_increment
-`SuperpowerID` int not null,
-
-PRIMARY KEY(`Character_SuperpowerID`)
+PRIMARY KEY(`Character_SuperPowerID`)
 );
-/**********************Character_Superpower***************
->>>>>>> sosaTest
+
+
+-- Character Table:
+CREATE TABLE IF NOT EXISTS `Character` (
+`CharacterID` int not null auto_increment,
+`Name` VARCHAR(45),
+`Description` VARCHAR(45),
+`IsSuperHero` BIT(1),
+
+PRIMARY KEY (`CharacterID`)
+
+);
+
+-- SuperPower Table:
+CREATE TABLE IF NOT EXISTS `SuperPower`(
+`SuperPowerID` int not null auto_increment,
+`SuperPowerType` VARCHAR(45),
+
+PRIMARY KEY (`SuperPowerType`)
+);
+
+
+
+
+
+/**********************Character_Superpower***************/
+ALTER TABLE `Character_SuperpowerID`
+ADD CONSTRAINT`fk_Character_Superpower_Character` FOREIGN KEY (`Character_SuperpowerID`) REFERENCES `Character`
+(`CharacterID`) ON DELETE NO ACTION;
+
+/*********************************************************/
