dee65d7842365ea3b2979b34f073f320ff59091a
MB <mbarry323@gmail.com>
2018-08-09 23:00:54 -0400

    LZMR-48 annotations to configure unit test dependency injection

--- Netbeans/SuperheroSightings/src/test/java/com/sg/superherosightings/dao/CharactersDaoUnitTest.java
+++ Netbeans/SuperheroSightings/src/test/java/com/sg/superherosightings/dao/CharactersDaoUnitTest.java
@@ -9,14 +9,38 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author laptop
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/test-applicationContext.xml"})
+@Rollback
+@Transactional
+
+/*
+Mo:
+
+@RunWith(SpringJUnit4ClassRunner.class): Indicates that the class should use Spring's JUnit facilities.
+
+@ContextConfiguration(locations = {...}): Indicates which XML files contain the ApplicationContext.
+
+@Rollback - when each test is complete, roll back all the changes performed
+
+@Transactional - keeps all the tests in one transaction so it can roll back 
+the entire test as one transaction
+*/
+
 public class CharactersDaoUnitTest {
+    
+    private CharactersDao charactersDao; //Inject the dao interface
     
     public CharactersDaoUnitTest() {
     }
