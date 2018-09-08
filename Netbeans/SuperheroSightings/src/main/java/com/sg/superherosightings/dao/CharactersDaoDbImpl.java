d34f254d32bdf07e242344bfb1bb2226ea0ad447
MB <mbarry323@gmail.com>
2018-09-08 19:37:34 -0400

    LZMR-48 mapRow for Characters in SightingDate

--- Netbeans/SuperheroSightings/src/main/java/com/sg/superherosightings/dao/CharactersDaoDbImpl.java
+++ Netbeans/SuperheroSightings/src/main/java/com/sg/superherosightings/dao/CharactersDaoDbImpl.java
@@ -53,8 +53,18 @@ public class CharactersDaoDbImpl implements CharactersDao {
     private static final String SQL_SELECT_ALL_HEROES
             = "select * from `Characters` where isSuperHero = 1";
 
-    private static final String SQL_SELECT_ALL_VILLAINS
-            = "select * from `Characters` where isSuperHero = 0";
+    private static final String SQL_SELECT_ALL_VILLAINS_JOIN_BY_SIGHTINGDATE
+            = "Select `CharacterName`, `SightingDate`, Characters.CharacterID from `Sighting`\n"
+            + "\n"
+            + "join  Characters\n"
+            + "\n"
+            + "On Characters.CharacterID = Sighting.CharacterID\n"
+            + "\n"
+            + "WHERE Characters.CharacterID = '?' and Characters.IsSuperHero='0'\n"
+            + "\n"
+            + "ORDER BY `SightingDate` DESC\n"
+            + "\n"
+            + "Limit 1;";
 
     private static final String SQL_SELECT_ALL_POWERS
             = "select * from `SuperPower`";
@@ -63,15 +73,19 @@ public class CharactersDaoDbImpl implements CharactersDao {
             = "select sp.SuperpowerId , sp.SuperPowerType from Superpower sp"
             + " Join Character_SuperPower cs on sp.SuperpowerID  = cS.SuperpowerId where CharacterId = ?";
 
-    private static final String SQL_SELECT_CHARACTER_BY_SIGHTINGDATE
-            = "SELECT * FROM SuperheroSightings.Sighting\n"
+    private static final String SQL_SELECT_HEROES_JOIN_BY_SIGHTINGDATE
+            = "Select `CharacterName`, `SightingDate`, Characters.CharacterID from `Sighting`\n"
             + "\n"
-            + "WHERE `CharacterID` = '?'\n"
+            + "join  Characters\n"
+            + "\n"
+            + "On Characters.CharacterID = Sighting.CharacterID\n"
+            + "\n"
+            + "WHERE Characters.CharacterID = '?' and Characters.IsSuperHero='1'\n"
             + "\n"
             + "ORDER BY `SightingDate` DESC\n"
             + "\n"
-            + "Limit 1";
-    
+            + "Limit 1;";
+
     @Override
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 
@@ -150,13 +164,13 @@ public class CharactersDaoDbImpl implements CharactersDao {
 
     @Override
     public List<Characters> getAllVillains() {
-        return jdbcTemplate.query(SQL_SELECT_ALL_VILLAINS,
+        return jdbcTemplate.query(SQL_SELECT_ALL_VILLAINS_JOIN_BY_SIGHTINGDATE,
                 new CharactersMapper());
     }
 
     @Override
     public List<Characters> getAllHeroes() {
-        return jdbcTemplate.query(SQL_SELECT_ALL_HEROES,
+        return jdbcTemplate.query(SQL_SELECT_HEROES_JOIN_BY_SIGHTINGDATE,
                 new CharactersMapper());
     }
 
@@ -192,20 +206,14 @@ public class CharactersDaoDbImpl implements CharactersDao {
             charact.setName(rs.getString("CharacterName"));
             charact.setDescription(rs.getString("Description"));
             charact.setIsSuperHero(rs.getBoolean("IsSuperHero"));
+            charact.setMostRecentSightingDate(rs.getDate("SightingDate"));
 
             return charact;
 
         }
     }
-    private static final class DateMapper implements RowMapper<Date> {
 
-        public Date mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Date myDate = new Date();
-           
-            return myDate;
-
-        }
-    }
+    
 
     private static final class SuperPowerMapper implements RowMapper<Power> {
 
